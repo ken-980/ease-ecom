0824f8b6fe3a3c30ae8d3cd723b0c89058d325c7
Kendrick Amankwah <kendrick.amankwah4@gmail.com>
2024-10-25 18:31:52 +0000

    working on participants view display

--- frontend/src/components/admin/dashboard/nav/nav-pages/sales/Sales.tsx
+++ frontend/src/components/admin/dashboard/nav/nav-pages/sales/Sales.tsx
@@ -5,8 +5,8 @@ export const Sales = () => {
     return(
         <div>
             <div className="p-4 rounded h-32 shadow bg-green-50 space-y-4">
-                <h5 className="text-gray-500 font-plus-font"> Total Inventory items </h5>
-                <h3 className="text-3xl font-bold font-plus-font"> 69 items </h3>
+                <h5 className="text-gray-500 font-plus-font"> Total Inventory volume </h5>
+                <h3 className="text-3xl font-bold font-plus-font"> 69 Products </h3>
             </div>
         </div>
     )
